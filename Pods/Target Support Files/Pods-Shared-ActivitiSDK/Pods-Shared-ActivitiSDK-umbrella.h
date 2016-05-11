f6953d7253aabc651cdcf7840f8153cc161b6f5f
Emanuel Lupu-Marinei <Emanuel.Lupu@ness.com>
2016-05-11 18:32:38 +0300

    #8 Prerequisites - Updated cocoapods support from 0.39 to 1.0 - Added AFOAuth2Manager extension for AFNetworking - rewrite for the pod configuration file to reflect 1.0 requirements

---
+++ Pods/Target Support Files/Pods-Shared-ActivitiSDK/Pods-Shared-ActivitiSDK-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Shared_ActivitiSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Shared_ActivitiSDKVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Shared-AlfrescoActiviti/Pods-Shared-AlfrescoActiviti-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Shared_AlfrescoActivitiVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Shared_AlfrescoActivitiVersionString[];
+
