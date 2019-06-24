c34802dcd9122ff556672961b61ddfc6ca5e9cd2
Florin Baincescu <florin.baincescu@ness.com>
2019-06-24 14:04:55 +0300

    IOS-1354: Update version and build numbers

--- AlfrescoActiviti/Resources/version-update.sh
+++ AlfrescoActiviti/Resources/version-update.sh
@@ -1,10 +1,10 @@
- #!/bin/bash
-VERSIONNUM=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
-SHORTVERSIONNUM=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PROJECT_DIR}/${INFOPLIST_FILE}")
-NEWSUBVERSION=`echo $VERSIONNUM`
-NEWSUBVERSION=$(($NEWSUBVERSION + 1))
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $NEWSUBVERSION" "${PROJECT_DIR}/${INFOPLIST_FILE}"
-
-SETTINGSBUNDLEPATH="${PROJECT_DIR}/${PRODUCT_NAME}/Resources/Settings.bundle/Root.plist"
-key=1
-/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:$key:DefaultValue '$SHORTVERSIONNUM ($NEWSUBVERSION)'" "$SETTINGSBUNDLEPATH"
+# #!/bin/bash
+#VERSIONNUM=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+#SHORTVERSIONNUM=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+#NEWSUBVERSION=`echo $VERSIONNUM`
+#NEWSUBVERSION=$(($NEWSUBVERSION + 1))
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $NEWSUBVERSION" "${PROJECT_DIR}/${INFOPLIST_FILE}"
+#
+#SETTINGSBUNDLEPATH="${PROJECT_DIR}/${PRODUCT_NAME}/Resources/Settings.bundle/Root.plist"
+#key=1
+#/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:$key:DefaultValue '$SHORTVERSIONNUM ($NEWSUBVERSION)'" "$SETTINGSBUNDLEPATH"
